71fcf14fcae2d26fe6b8bb567a05f2deaf41b0de
norikois <33947244+norikois@users.noreply.github.com>
2024-03-19 15:12:39 +0900

    Initial code commit

---
+++ KB13213_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using KB13213_WpfApp1.Model;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KB13213_WpfApp1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void series1_AssigningCategoryStyle(object sender, Infragistics.Controls.Charts.AssigningCategoryStyleEventArgs args)
+    {
+        // 点数が 80 点以上は緑
+        if (((ScoreResult)args.GetItems(args.StartIndex, args.EndIndex)[0]).Score >= 80)
+        {
+            args.Fill = new SolidColorBrush(Colors.LightGreen);
+            args.Stroke = new SolidColorBrush(Colors.Green);
+        }
+        // 点数が 80 点未満 50 点以上は黄色
+        else if (((ScoreResult)args.GetItems(args.StartIndex, args.EndIndex)[0]).Score >= 50)
+        {
+            args.Fill = new SolidColorBrush(Colors.LightYellow);
+            args.Stroke = new SolidColorBrush(Colors.Yellow);
+        }
+        // 点数が 50 点未満は赤
+        else
+        {
+            args.Fill = new SolidColorBrush(Colors.Pink);
+            args.Stroke = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
